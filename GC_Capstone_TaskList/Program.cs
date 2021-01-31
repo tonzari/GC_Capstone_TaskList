85ab5e1501c04315de6a3095b9189aa48257390a
antonio <antoniomanzari@gmail.com>
2021-01-31 15:08:46 -0500

    init commit

---
+++ GC_Capstone_TaskList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GC_Capstone_TaskList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
